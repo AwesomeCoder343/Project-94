011964ec2aeb0549b852d04dd061f0ea42193f89
AwesomeCoder343 <73550340+AwesomeCoder343@users.noreply.github.com>
2020-11-20 07:25:28 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDhRNo_GMl1uIs1cUVr05yi0QC7ZqV6_mQ",
+    authDomain: "kwitter-2f8c5.firebaseapp.com",
+    databaseURL: "https://kwitter-2f8c5.firebaseio.com",
+    projectId: "kwitter-2f8c5",
+    storageBucket: "kwitter-2f8c5.appspot.com",
+    messagingSenderId: "742079310947",
+    appId: "1:742079310947:web:f2dcf3733a33743fc31c56"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
